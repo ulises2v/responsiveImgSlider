da40f60da14501113279a23d3dd9947f8818922a
Christian Velazquez - u <chrisul2v@gmail.com>
2021-06-11 17:45:32 -0500

    agregano funcion 1 a menu slider e iconos RS

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Add active class in selected thumbails
+
+let el = document.querySelectorAll('.thumb li');
+
+for (let i = 0; i < el.length; i++) {
+    el[i].onclick = () => {
+        let c = 0;
+        while (c < el.length) {
+            el[c++].className = 'check';
+        }
+        el[i].className = 'check active';
+    }
+}
